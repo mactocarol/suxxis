778bbeec34cac632b560cd9410df34a5f14e2371
mactocarol <42925919+mactocarol@users.noreply.github.com>
2018-12-21 14:48:03 +0530

    Update config.php

--- application/config.php
+++ application/config.php
@@ -11,19 +11,19 @@ $config =array(
 
 			"Google" => array ( 
 				"enabled" => true,
-				"keys"    => array ( "id" => "797353433483-g68hru756k4sngq5qmoujhp7qhor1tt8.apps.googleusercontent.com",
+				"keys"    => array ( "id" => "-.apps.com",
 				"secret" => "Y7tlZCahTd2iMju7_UAjfwbg" ), 
 			),
 
 			"Facebook" => array ( 
 				"enabled" => true,
-				"keys"    => array ( "id" => "799366307076656", "secret" => "129380feed699b8644fd2c6fe6c2c4e0" ),
+				"keys"    => array ( "id" => "", "secret" => "" ),
                 "scope"   => ['email', '', '', ''], // optiona				
 			),
 
 			"Twitter" => array ( 
 				"enabled" => true,
-				"keys"    => array ( "key" => "uhRsWpVuv5cNDy4KMOZuu9ydF", "secret" => "DZGISBfczI1itjYzKI7ziU6cddxrcEUw0LtVdciZjKvkD4kSeH" ), 
+				"keys"    => array ( "key" => "", "secret" => "" ), 
 			     "includeEmail" => true
 			),
 			/*
